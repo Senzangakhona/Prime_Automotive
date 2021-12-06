a25cb5086e5d83c6b01c30add669502641ec1ae5
Senzangakhona <63323689+Senzangakhona@users.noreply.github.com>
2021-12-06 09:14:58 -0300

    Update UploadImagem.dart

--- lib/cadsatro/UploadImagem.dart
+++ lib/cadsatro/UploadImagem.dart
@@ -1,1 +1,75 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ImagePicker imagePicker = ImagePicker();
+  File? imagemSelecionada;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Imagens'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          imagemSelecionada == null
+              ? Container()
+              : Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Image.file(imagemSelecionada!),
+                ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  pegarImagemGaleria();
+                },
+                icon: Icon(Icons.add_photo_alternate_outlined),
+              ),
+              IconButton(
+                onPressed: () {
+                  pegarImagemCamera();
+                },
+                icon: Icon(Icons.photo_camera_outlined),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  pegarImagemGaleria() async {
+    PickedFile? imagemTemporaria =
+        await imagePicker.getImage(source: ImageSource.gallery);
+    if (imagemTemporaria != null) {
+      setState(
+        () => imagemSelecionada = File(imagemTemporaria.path),
+      );
+    }
+  }
+
+  pegarImagemCamera() async {
+    PickedFile? imagemTemporaria =
+        await imagePicker.getImage(source: ImageSource.camera);
+    if (imagemTemporaria != null) {
+      setState(
+        () {
+          imagemSelecionada = File(imagemTemporaria.path);
+        },
+      );
+    }
+  }
+}
